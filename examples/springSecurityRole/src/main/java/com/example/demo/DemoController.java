1774b9069ad5a9f6b0ba13a69c379849f0da4d1e
BlackB34rd <contact@the-tiny-spark.com>
2025-12-19 10:00:29 +0100

    add spring security RBAC example

---
+++ examples/springSecurityRole/src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/public")
+    public String publicEndpoint() {
+        return "Public access";
+    }
+
+    @GetMapping("/user")
+    public String userEndpoint() {
+        return "User access";
+    }
+
+    @GetMapping("/admin")
+    public String adminEndpoint() {
+        return "Admin access";
+    }
+}
